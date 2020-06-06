75a299b879830d344bc31fb6634f52e7ac61067c
Jeffrey Bergier <jbergier@mercari.com>
2020-06-06 17:01:10 +0900

    Added Reading and Writing from Cache

---
+++ CCCC/CCCC/CacheReadWrite.swift
@@ -0,0 +1,71 @@
+//
+//  CacheReadWrite.swift
+//  CCCC
+//
+//  Created by Jeffrey Bergier on 2020/06/06.
+//  Copyright © 2020 Jeffrey Bergier.
+//
+//  MIT License
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Combine
+import Foundation
+
+private let kCacheURL: URL = {
+    let cache = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    return cache.appendingPathComponent("CCCC_cache.plist")
+}()
+
+let cacheRead: Cacher<Data>.CacheRead = {
+    Future { promise in
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                let data = try Data(contentsOf: kCacheURL)
+                let decode = try PropertyListDecoder().decode(Cacher<Data>.Cache.self, from: data)
+                DispatchQueue.main.async {
+                    promise(.success(decode))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    promise(.failure(error))
+                }
+            }
+        }
+    }
+}
+
+let cacheWrite: Cacher<Data>.CacheWrite = { cache in
+    Future { promise in
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                let data = try PropertyListEncoder().encode(cache)
+                try data.write(to: kCacheURL)
+                DispatchQueue.main.async {
+                    promise(.success(()))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    promise(.failure(error))
+                }
+            }
+        }
+    }
+}
